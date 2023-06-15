78b6e9286a746409a8750281cb183c6c11c3d817
Maricel <cerezomaricel04@gmail.com>
2023-06-15 22:06:35 +0800

    commit hide env file

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,8 +4,11 @@ import GitHubProvider from "next-auth/providers/github";
 export default NextAuth({
     providers: [
         GitHubProvider({
+            // **** If key is in env file. ****
             // clientId: `${process.env.GITHUB_ID}`,
             // clientSecret: `${process.env.GITHUB_SECRET}`,
+
+            // **** Key should be hidden from the public. But for demonstration purpose I hardcode it ****
             clientId: 'c9260d32970a3ce353ea',
             clientSecret: '24be1f0191c00ea9df304d88ffcaffda072c0bb7',
         })
