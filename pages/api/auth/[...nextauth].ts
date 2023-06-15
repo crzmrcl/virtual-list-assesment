8d034e67a05cf82d4733eb13c5f3bfd6d749b9a1
Maricel <cerezomaricel04@gmail.com>
2023-06-15 21:04:41 +0800

    commit hardcode id and secret for testing

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,8 +4,10 @@ import GitHubProvider from "next-auth/providers/github";
 export default NextAuth({
     providers: [
         GitHubProvider({
-            clientId: `${process.env.GITHUB_ID}`,
-            clientSecret: `${process.env.GITHUB_SECRET}`,
+            // clientId: `${process.env.GITHUB_ID}`,
+            // clientSecret: `${process.env.GITHUB_SECRET}`,
+            clientId: 'c9260d32970a3ce353ea',
+            clientSecret: '24be1f0191c00ea9df304d88ffcaffda072c0bb7',
         })
     ]
 })
